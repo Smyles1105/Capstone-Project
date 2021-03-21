15bde3fc02eb6f5c56158fa6c58bd70db1516726
Smyles1105 <mylesw6575@gmail.com>
2021-03-21 13:22:13 -0700

    Initial commit

---
+++ express/dragged.js
@@ -0,0 +1,30 @@
+
+document.addEventListener("mousemove", drag);
+document.addEventListener("mouseup", dragEnd);
+
+let draggingElement = null,
+	grabX = null,
+	grabY = null;
+
+function dragStart(e) {
+	draggingElement = e.target.parentElement;
+    const boundingBox = draggingElement.getBoundingClientRect();
+    
+	grabX = e.clientX - boundingBox.left;
+	grabY = e.clientY - boundingBox.top;
+}
+
+function dragEnd() {
+	draggingElement = null;
+}
+
+function drag(e) {
+	if (draggingElement !== null) {
+		const newX = e.clientX - grabX,
+			  newY = e.clientY - grabY;
+
+		draggingElement.style.top = newY + "px";
+		draggingElement.style.left = newX + "px";
+		draggingElement.style.position = "absolute";
+	}
+}
---
+++ express/task.js
@@ -0,0 +1,104 @@
+document.getElementById("add").addEventListener("click", addTask);
+
+let taskID = 0;
+
+function deleteTask()
+{
+	this.parentElement.remove();
+}
+
+	const colourInput = document.createElement("input");
+	colourInput.type = "color";
+	colourInput.addEventListener("oninput", taskBackgroundChange);
+
+
+function taskBackgroundChange(t, c)
+{
+	t.style.backgroundColor = c.value;
+
+}
+
+
+function addTask() {
+	const task = document.createElement("div");
+	task.classList.add("task");
+	
+
+	const taskTitle = document.createElement("h2");
+	taskTitle.appendChild(document.createTextNode(`Task #${++taskID}`));
+	taskTitle.contentEditable = true;
+
+	const content = document.createElement("div");
+	content.classList.add("content");
+	content.appendChild(taskTitle);
+
+	const taskHeader = document.createElement("div");
+	taskHeader.classList.add("taskHeader");
+	taskHeader.addEventListener("mousedown", dragStart);
+
+	const deleteButton = document.createElement("button");
+	deleteButton.classList.add("deleteButton");
+
+	const delButIcon = document.createElement("i");
+	delButIcon.classList.add("fas", "fa-trash");
+	deleteButton.appendChild(delButIcon);
+	deleteButton.addEventListener("mousedown", deleteTask);
+
+	const textArea = content.appendChild(document.createElement("textarea"));
+	textArea.textContent = ("Click here to type...");
+
+	
+	task.appendChild(taskHeader);
+	task.appendChild(content);
+	task.appendChild(deleteButton);
+	
+
+	
+	tinymce.init({
+		selector: '.task textarea',
+		plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+		toolbar_mode: 'floating',
+		menubar: 'false',
+		elementpath: 'false',
+		wordcount: 'false',
+		toolbar: 'text | colour',
+		setup: function (editor) {
+			editor.ui.registry.addButton('taskbackcolour',
+			{
+			icon: 'fill',
+			tooltip: 'Change task background colour',
+			onAction: function () {
+
+				colourInput.click();
+				colourInput.addEventListener("oninput", taskBackgroundChange);
+				taskBackgroundChange(task, colourInput);
+			}
+			});
+		},
+		toolbar_groups: 
+		{
+			text: 
+			{
+				icon: 'change-case',
+				tooltip: 'Text tools',
+				items: 'bold italic underline strikethrough | superscript subscript | align | text-color highlight-bg-color'
+			},
+			colour:
+			{
+				icon: 'color-picker',
+				tooltip: 'Background Colours',
+				items: 'backcolor | taskbackcolour'
+			}
+		}
+	 });
+
+	 document.getElementById("tasks").appendChild(task);
+}
+
+addTask();
+addTask();
+addTask();
+addTask();
+
+
+
